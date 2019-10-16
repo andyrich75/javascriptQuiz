62966fd2c12fe27529b90b069d4668fe901acdb3
andyrich75 <arichard101@gmail.com>
2019-10-16 18:19:28 -0400

    added files and script.js

---
+++ assets/questions.js
@@ -0,0 +1,28 @@
+// Setting the question with an array
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"  
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: [ "javascript", "js", "scripting", "script"],
+        answer: "script"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function:myFunction()", "function myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "How do you call a function named "myFunction"?",
+        choices: "call myFunction()", "myFunction()", "call function myFunction()",
+        answer: "myFunction()"
+    }
+]
