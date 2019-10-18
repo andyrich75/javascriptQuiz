0868d621bdf52f6943557a9c31cd28fa5a38fb64
andyrich75 <arichard101@gmail.com>
2019-10-18 15:29:29 -0400

    added some script

--- assets/questions.js
+++ assets/questions.js
@@ -1,3 +1,45 @@
+function populate() {
+    if(quiz.isEnded()); {
+        showScore();
+
+    }
+
+    else {
+        var element = document.getElementById("questions");
+        element.innerHTML = quiz.getQuestionsIndex().text;
+
+        var choices = quiz.getQuestionsIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choices" + i);
+            ekement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+        showProgress();
+    }
+};
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick - function() {
+        quiz.guess(guess);
+        populate();
+    }
+}
+
+function showProgress() {
+    var currentQuestionNum = quiz.questionsIndex + 1;
+    var elemnt = document.getElementById("progress");
+    element.innerHTML = "Questions" + currentQuestionNum + "of" + quiz.questions.length;
+}
+ function showScore() {
+     var gameOverHTML = "<h1>Result</h1>";
+         gameOverHTML += "<h2 id="score"> Your Score: " + quiz.score + "</h2>";
+         var element = document.getElementById("quiz");
+         element.innerHTML = gameOverHTML;
+ };
+
+
+
+
 // Setting the question with an array
 var questions = [
     {
@@ -17,12 +59,16 @@ var questions = [
     },
     {
         title: "How do you create a function in JavaScript?",
-        choices: ["function = myFunction()", "function:myFunction()", "function myFunction()"],
+        choices: ["function = myFunction()", "function:myFunction()", "function myFunction()", "Any of the Above"],
         answer: "function myFunction()"
     },
     {
         title: "How do you call a function named "myFunction"?",
-        choices: "call myFunction()", "myFunction()", "call function myFunction()",
+        choices: ["call myFunction()", "myFunction()", "call function myFunction()", "Any of the Above"]
         answer: "myFunction()"
     }
-]
+];
+ 
+var quiz = new Quiz(questions);
+
+populate();
---
+++ assets/script.js
@@ -0,0 +1,32 @@
+//function for the quiz to run and pull answer with adding a score.
+function Quiz(questions) {
+    this.score =0;
+    this.questions = questions;
+    this.questionsIndex = 0;
+}
+
+Quiz.prototype.getQuestionsIndex = function() {
+    return this.questions[this.questionsIndex];
+
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questions.length === this.questionsIndex;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionsIndex().correcAnswer(answer)) {
+        this.score++;
+    }
+    this.questionsIndex++;
+}
+//function for the questions to be called
+function questions(title, choices, answer) {
+    this.title = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+questions.prototype.correctAnswer = function(choices) {
+    return choices === this.answer;
+}
