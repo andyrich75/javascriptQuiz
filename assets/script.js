5257c8a8a7891aed592439749703702460ed88e9
andyrich75 <arichard101@gmail.com>
2019-10-29 08:02:29 -0400

    added some snippit of JS code to start and hide button

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,8 @@
+//begins quiz and then removes the start button 
+//grabbed this from a snippit of code that I found googling
+var start = document.getElementById("start-quiz");
+start.addEventListener("click", function(){
+  start.parentNode.removeChild(start);
+  runQuiz();
+})
 
-//function for the quiz to run and pull answer with adding a score.
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionsIndex = 0;
-}
-
-quiz.prototype.getQuestionsIndex = function() {
-    return this.questions[this.questionsIndex];
-
-}
-
-quiz.prototype.isEnded = function() {
-    return this.questions.length === this.questionsIndex;
-}
-
-quiz.prototype.guess = function(answer) {
-    if(this.getQuestionsIndex().correcAnswer(answer)) {
-        this.score++;
-    }
-    this.questionsIndex++;
-}
-//function for the questions to be called
-function questions(title, choices, answer) {
-    this.title = text;
-    this.choices = choices;
-    this.answer = answer;
-}
-
-questions.prototype.correctAnswer = function(choices) {
-    return choices === this.answer;
-}
-  console.log(choices)
